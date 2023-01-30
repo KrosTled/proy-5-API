f701771010618c1cad094ce24d9c10c405a7017c
KrosTled <maig.pabu@gmail.com>
2023-01-30 03:09:21 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const { Auth, estaAutenticada } = require('./userUse')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 
 app.post('/login', Auth.login)
